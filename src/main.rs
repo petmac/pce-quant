97c80813888e2e2ab662f8d0ca8aefb257d26d5a
Peter Mackay <git@petmac.dev>
2024-04-10 16:32:02 +0100

    Parse command line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    input_path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("Input: {}", cli.input_path.display());
 }
