689f8e0a9738dbfb37f30d1d2a26eaf8b807880b
Peter Mackay <git@petmac.dev>
2024-04-10 17:14:16 +0100

    Move decode to true_color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod true_color;
 
-use std::{error::Error, fs::File, path::PathBuf};
+use std::{error::Error, path::PathBuf};
 
 use clap::Parser;
-use png::{Decoder, Transformations};
-use true_color::{TrueColor, TrueColorImage};
+use true_color::TrueColorImage;
 
 #[derive(Parser)]
 struct Cli {
@@ -15,22 +14,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     println!("Input: {}", cli.input_path.display());
 
-    let input_image = decode(&cli.input_path)?;
+    let input_image = TrueColorImage::decode(&cli.input_path)?;
 
     Ok(())
-}
-
-fn decode(path: &PathBuf) -> Result<TrueColorImage, Box<dyn Error>> {
-    let mut decoder = Decoder::new(File::open(path)?);
-    decoder.set_transformations(Transformations::EXPAND);
-    let mut reader = decoder.read_info()?;
-    let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf)?;
-    let pixels = buf.chunks_exact(3).map(TrueColor::new).collect();
-
-    Ok(TrueColorImage {
-        width: info.width as usize,
-        height: info.height as usize,
-        pixels,
-    })
 }
--- src/true_color.rs
+++ src/true_color.rs
@@ -1,7 +1,28 @@
+use std::{error::Error, fs::File, path::PathBuf};
+
+use png::{Decoder, Transformations};
+
 pub struct TrueColorImage {
     pub width: usize,
     pub height: usize,
     pub pixels: Vec<TrueColor>,
+}
+
+impl TrueColorImage {
+    pub fn decode(path: &PathBuf) -> Result<TrueColorImage, Box<dyn Error>> {
+        let mut decoder = Decoder::new(File::open(path)?);
+        decoder.set_transformations(Transformations::EXPAND);
+        let mut reader = decoder.read_info()?;
+        let mut buf = vec![0; reader.output_buffer_size()];
+        let info = reader.next_frame(&mut buf)?;
+        let pixels = buf.chunks_exact(3).map(TrueColor::new).collect();
+
+        Ok(TrueColorImage {
+            width: info.width as usize,
+            height: info.height as usize,
+            pixels,
+        })
+    }
 }
 
 pub struct TrueColor {
