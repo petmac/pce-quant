ae3922d6c3176611a99d94d9b16dc797b583e9eb
Peter Mackay <git@petmac.dev>
2024-04-11 10:01:05 +0100

    Compute weighted average color

--- src/quantize.rs
+++ src/quantize.rs
@@ -4,6 +4,7 @@ use crate::{color::Rgb8, indexed::IndexedImage, true_color::TrueColorImage};
 
 pub fn quantize(input_image: &TrueColorImage) -> IndexedImage {
     let color_counts = count_unique_colors(input_image);
+    let avg_color = weighted_average_color(&color_counts);
     let palette: Vec<Rgb8> = input_image.pixels[0..16].iter().copied().collect();
     let pixels = vec![0; input_image.pixels.len()];
 
@@ -28,4 +29,21 @@ fn count_unique_colors(image: &TrueColorImage) -> BTreeMap<Rgb8, usize> {
     }
 
     color_counts
+}
+
+fn weighted_average_color(color_counts: &BTreeMap<Rgb8, usize>) -> (f64, f64, f64) {
+    let mut r = 0;
+    let mut g = 0;
+    let mut b = 0;
+    let mut denominator = 0;
+
+    for (color, &count) in color_counts {
+        r += color.r as usize;
+        g += color.g as usize;
+        b += color.b as usize;
+        denominator += count;
+    }
+
+    let scale = 1.0 / denominator as f64;
+    (r as f64 * scale, g as f64 * scale, b as f64 * scale)
 }
