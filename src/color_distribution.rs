0c6477a9ba09a96c4c384bbf5a3e853752d1c674
Peter Mackay <git@petmac.dev>
2024-04-15 22:31:27 +0100

    Simplify color distribution for better clustering

--- src/color_distribution.rs
+++ src/color_distribution.rs
@@ -1,48 +1,50 @@
-use std::collections::BTreeMap;
-
 use clustering::Elem;
 
 use crate::color::ColorU8;
 
 pub struct ColorDistribution {
-    color_pixel_counts: BTreeMap<ColorU8, usize>,
+    min: ColorU8,
+    max: ColorU8,
 }
 
 impl ColorDistribution {
     pub fn new(pixels: &[ColorU8]) -> ColorDistribution {
-        let mut color_pixel_counts = BTreeMap::new();
-
-        for color in pixels {
-            match color_pixel_counts.get_mut(color) {
-                Some(pixel_count) => *pixel_count += 1,
-                None => {
-                    color_pixel_counts.insert(*color, 1);
-                }
-            }
+        let min_r = pixels.iter().map(red).min().unwrap_or_default();
+        let min_g = pixels.iter().map(green).min().unwrap_or_default();
+        let min_b = pixels.iter().map(blue).min().unwrap_or_default();
+        let max_r = pixels.iter().map(red).max().unwrap_or_default();
+        let max_g = pixels.iter().map(green).max().unwrap_or_default();
+        let max_b = pixels.iter().map(blue).max().unwrap_or_default();
+        ColorDistribution {
+            min: ColorU8 {
+                r: min_r,
+                g: min_g,
+                b: min_b,
+            },
+            max: ColorU8 {
+                r: max_r,
+                g: max_g,
+                b: max_b,
+            },
         }
-
-        ColorDistribution { color_pixel_counts }
     }
 }
 
 impl Elem for ColorDistribution {
     fn dimensions(&self) -> usize {
-        3
+        6
     }
 
     fn at(&self, i: usize) -> f64 {
-        let channel_selector = match i {
-            0 => red,
-            1 => green,
-            _ => blue,
+        let component = match i {
+            0 => self.min.r,
+            1 => self.min.g,
+            2 => self.min.b,
+            3 => self.max.r,
+            4 => self.max.g,
+            _ => self.max.b,
         };
-
-        let pixel_counts_for_dimension: usize = self
-            .color_pixel_counts
-            .iter()
-            .map(|(color, &pixel_count)| channel_selector(color) as usize * pixel_count)
-            .sum();
-        pixel_counts_for_dimension as f64
+        component as f64
     }
 }
 
