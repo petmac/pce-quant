783d79da42c4620fce45d90b64a45e7f7a4a4eef
Peter Mackay <git@petmac.dev>
2024-04-16 00:34:56 +0100

    WIP error correction

--- src/remap.rs
+++ src/remap.rs
@@ -6,10 +6,24 @@ use crate::{
 
 pub fn remap_tile(ideal_tile: &Tile, palette: &[Color]) -> IndexedPattern {
     let mut pattern = IndexedPattern::default();
+    let mut error = [0.0; 3];
 
     for y in 0..TILE_SIZE {
         for x in 0..TILE_SIZE {
-            pattern[y][x] = nearest_color_in_palette(&ideal_tile[y][x], palette) as u8;
+            let ideal_color = &ideal_tile[y][x];
+            let target_color = Color {
+                r: ideal_color.r - error[0],
+                g: ideal_color.g - error[1],
+                b: ideal_color.b - error[2],
+            };
+            let nearest_color_index = nearest_color_in_palette(&target_color, palette);
+            let nearest_color = &palette[nearest_color_index];
+
+            error[0] = nearest_color.r - target_color.r;
+            error[1] = nearest_color.g - target_color.g;
+            error[2] = nearest_color.b - target_color.b;
+
+            pattern[y][x] = nearest_color_index as u8;
         }
     }
 
