9426998b490971ac5612eae81115b9d4395c82f5
Peter Mackay <git@petmac.dev>
2024-04-16 15:18:13 +0100

    Add remap_pixel function

--- src/remap.rs
+++ src/remap.rs
@@ -4,8 +4,6 @@ use crate::{
     tiled_indexed_image::IndexedPattern,
 };
 
-const ERROR_CORRECTION: f64 = 0.25;
-
 pub fn remap_tile(ideal_tile: &Tile, palette: &[Color]) -> IndexedPattern {
     let mut pattern = IndexedPattern::default();
 
@@ -14,23 +12,30 @@ pub fn remap_tile(ideal_tile: &Tile, palette: &[Color]) -> IndexedPattern {
 
         for x in 0..TILE_SIZE {
             let ideal_color = &ideal_tile[y][x];
-            let target_color = Color {
-                r: ideal_color.r - error[0] * ERROR_CORRECTION,
-                g: ideal_color.g - error[1] * ERROR_CORRECTION,
-                b: ideal_color.b - error[2] * ERROR_CORRECTION,
-            };
-            let nearest_color_index = nearest_color_in_palette(&target_color, palette);
-            let nearest_color = &palette[nearest_color_index];
-
-            error[0] = nearest_color.r - target_color.r;
-            error[1] = nearest_color.g - target_color.g;
-            error[2] = nearest_color.b - target_color.b;
-
-            pattern[y][x] = nearest_color_index as u8;
+            pattern[y][x] = remap_pixel(ideal_color, palette, &mut error);
         }
     }
 
     pattern
+}
+
+fn remap_pixel(ideal_color: &Color, palette: &[Color], error: &mut [f64; 3]) -> u8 {
+    const ERROR_CORRECTION: f64 = 0.25;
+
+    let target_color = Color {
+        r: ideal_color.r - error[0] * ERROR_CORRECTION,
+        g: ideal_color.g - error[1] * ERROR_CORRECTION,
+        b: ideal_color.b - error[2] * ERROR_CORRECTION,
+    };
+
+    let nearest_color_index = nearest_color_in_palette(&target_color, palette);
+    let nearest_color = &palette[nearest_color_index];
+
+    error[0] = nearest_color.r - target_color.r;
+    error[1] = nearest_color.g - target_color.g;
+    error[2] = nearest_color.b - target_color.b;
+
+    nearest_color_index as u8
 }
 
 fn nearest_color_in_palette(ideal_color: &Color, palette: &[Color]) -> usize {
