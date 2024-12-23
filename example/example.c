64d033a020b0c0a52574f32701155065a46b46a4
Peter Mackay <git@petmac.dev>
2024-12-23 00:38:48 +0000

    Use cd_fastvram rather than cd_loadvram

--- example/example.c
+++ example/example.c
@@ -7,6 +7,7 @@
 #define BATM_SIZE (64 * 32 * 2)
 #define CHR_SIZE ((320 / 8) * (256 / 8) * 32)
 #define VRAM_SIZE (BATM_SIZE + CHR_SIZE)
+#define VRAM_SIZE_SECTORS ((VRAM_SIZE + 2047) >> 11)
 
 static int palettes[16 * 16];
 
@@ -15,8 +16,7 @@ void main() {
   scroll(0, 0, 8, 0, 223, 0xc0);
   cd_loaddata(OVL_PALETTE, 0, palettes, sizeof(palettes));
   set_bgpal(0, palettes, 16);
-  // cd_loadvram(int ovl_idx, int sector_offset, int vram_addr, int bytes)
-  cd_loadvram(OVL_VIDEODATA, 0, 0, VRAM_SIZE);
+  cd_fastvram(OVL_VIDEODATA, 0, 0, VRAM_SIZE_SECTORS);
 
   for (;;) {
     vsync();
